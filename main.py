73e1f63dd05397534e27a0ce8855bc64d982b1fe
IVAN <ivanvasilkov2004@mail.ru>
2023-01-29 11:20:47 +0300

    sova

--- main.py
+++ main.py
@@ -1,33 +1,77 @@
-from tkinter import *
+import tkinter
+import random
+from tkinter import PhotoImage
+
+def prepare_and_start():
+    global player
+    canvas.delete("all")
+    player_pos = (random.randint(1, N_X - 1) * step,
+                    random.randint(1, N_Y - 1) * step)
+    oval_pos = (random.randint(0, N_X - 1) * step,
+                random.randint(0, N_Y - 1) * step)
+
+    player = canvas.create_image(
+        (player_pos[0], player_pos[1]),
+        image=player_pic, anchor='nw')
+    label.config(text="Найди выход!")
+    master.bind("<KeyPress>", key_pressed)
 
 
-def move_by_keys(event):
+def move_warp(obj, move_x, move_y):
+    xy = canvas.coords(obj)
+    canvas.move(obj, move_x, move_y)
+    print(xy)
+    if xy[0] <= 0:
+        canvas.move(obj, WIDTH, 0)
+    if xy[0] >= 0:
+        canvas.move(obj, -WIDTH, 0)
+    if xy[1] <= 0:
+        canvas.move(obj, 0, HEIGHT)
+    if xy[1] >= 0:
+        canvas.move(obj, 0, -HEIGHT)
+
+def key_pressed(event):
     if event.keysym == 'Up':
-        canvas.move(rect, 0, -20)
-    elif event.keysym == 'Down':
-        canvas.move(rect, 0, 20)
-    elif event.keysym == 'Left':
-        canvas.move(rect, -20, 0)
-    elif event.keysym == 'Right':
-        canvas.move(rect, 20, 0)
+        move_warp(player, 0, -step)
+    if event.keysym == 'Down':
+        move_warp(player, 0, step)
+    if event.keysym == 'Right':
+        move_warp(player, step, 0)
+    if event.keysym == 'Left':
+        move_warp(player, -step, 0)
 
 
-win = Tk()
-label = Label(win, text='Побег из игры')
+master = tkinter.Tk()
+
+step = 10
+N_X = 50
+N_Y = 50
+WIDTH = step * N_X
+HEIGHT = step * N_Y
+a = False
+player_pic = tkinter.PhotoImage(file="player.png")
+canvas = tkinter.Canvas(master, bg='gray',
+                            width=600, height=600)
+canvas.create_oval(300, 300, )
+
+
+player_pos = (random.randint(0, N_X - 1) * step,
+                random.randint(0, N_Y - 1) * step)
+oval_pos = (random.randint(0, N_X - 1) * step,
+            random.randint(0, N_Y - 1) * step)
+
+print(oval_pos)
+print(player_pos)
+
+label = tkinter.Label(master, text="Не попадись!")
+restart = tkinter.Button(master, text="Начать заново", command=prepare_and_start)
+
+restart.pack()
 label.pack()
-canvas = Canvas(win, bg='#fff', width=700, height=700)
-rect = canvas.create_rectangle((50, 650), (150, 550), fill='red')
-oval = canvas.create_oval((330, 330), (400, 400), fill='purple')
 canvas.pack()
-win.bind("<KeyPress>", move_by_keys)
-def rect_and_oval(d, s):
-    if d == s:
-        win.destroy()
-
-rect_and_oval(rect, oval)
-
-win.mainloop()
-
+prepare_and_start()
+master.bind("<KeyPress>", key_pressed)
+master.mainloop()
 
 
 
